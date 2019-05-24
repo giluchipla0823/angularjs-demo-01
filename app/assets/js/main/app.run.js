89abf4c664b8cc4121733228089ddfbb811f8e55
Gino Chirinos <gino.chirinos@ticmash.com>
2019-05-24 14:59:26 +0200

    Agregando plugin auto-validate para formulario de contacto

--- app/assets/js/main/app.run.js
+++ app/assets/js/main/app.run.js
@@ -1,26 +1,22 @@
 (function(){
     angular.module('app.run', ['app.constans'])
         .run([
-			'validator',
-			'bootstrap3ElementModifier',
-			'defaultErrorMessageResolver',
-			function (validator, bootstrap3ElementModifier, defaultErrorMessageResolver) {
-				validator.setValidElementStyling(false);
-		        validator.setInvalidElementStyling(true);
-		      	bootstrap3ElementModifier.enableValidationStateIcons(false);
+					'validator',
+					'bootstrap3ElementModifier',
+					'defaultErrorMessageResolver',
+					function (validator, bootstrap3ElementModifier, defaultErrorMessageResolver) {
+							validator.setValidElementStyling(false);
+								validator.setInvalidElementStyling(true);
+								bootstrap3ElementModifier.enableValidationStateIcons(false);
 
-		      	defaultErrorMessageResolver.getErrorMessages().then(function(errorMessages) {
-			      errorMessages['ngPatternEmail'] = 'Please enter a valid email address.';
-			    })
+								defaultErrorMessageResolver.getErrorMessages().then(function(errorMessages) {
+								errorMessages['ngPatternEmail'] = 'Please enter a valid email address.';
+							})
 
-			    // console.log(window.location.origin);
+							var path = window.location.origin + document.getElementById('base-app').getAttribute('href');
 
-			    var path = window.location.origin + document.getElementById('base-app').getAttribute('href');
-
-			    // console.log(path);
-
-		      	defaultErrorMessageResolver.setI18nFileRootPath(path + 'assets/js/libs/auto-validate/lang');
-		      	defaultErrorMessageResolver.setCulture('es-co');
-		}]);
+							defaultErrorMessageResolver.setI18nFileRootPath(path + 'assets/js/libs/auto-validate/lang');
+							defaultErrorMessageResolver.setCulture('es-co');
+				}]);
 })();
 
